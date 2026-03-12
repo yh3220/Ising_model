53c044e7c5c47ce34b5d8096ebcc8c5f7b7301c7
yh3220 <144172243+yh3220@users.noreply.github.com>
2026-03-12 14:23:01 -0400

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,159 @@
+let screen_width = window.innerWidth, screen_height = window.innerHeight;
+let canvas_width, canvas_height;
+let fps = 24, paused = false;
+let mobile;
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    mobile = true;
+} else {
+    mobile = false;
+}
+
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+let pause_button = document.getElementById("pause-button");
+
+let spin_display = document.getElementById("spin-display");
+let energy_display = document.getElementById("energy-display");
+
+let size_input = document.getElementById("size-input");
+let size_display = document.getElementById("size-display");
+
+let rate_input = document.getElementById("rate-input");
+let rate_display = document.getElementById("rate-display");
+
+let temp_input = document.getElementById("temp-input");
+let temp_display = document.getElementById("temp-display");
+
+let j_input = document.getElementById("j-input");
+let j_display = document.getElementById("j-display");
+
+let h_input = document.getElementById("h-input");
+let h_display = document.getElementById("h-display");
+
+if (mobile) {
+    canvas_width = 0.9 * screen_width;
+}
+else {
+    canvas_width = 0.45 * screen_width;
+}
+canvas_height = canvas_width;
+
+canvas.width = canvas_width;
+canvas.height = canvas_height;
+
+let animate = window.requestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || function (callback) {
+        window.setTimeout(callback, 1000 / fps);
+    };
+
+function step() {
+    if (!paused) {
+        update();
+    }
+    animate(step);
+}
+
+window.onload = function () {
+    defaultParams();
+    initParams();
+    animate(step);
+}
+
+
+function defaultParams() {
+    size_input.value = 100;
+    rate_input.value = 1;
+    temp_input.value = 1;
+    j_input.value = 1;
+    h_input.value = 0;
+}
+
+let click_x, click_y, pressed;
+
+if(mobile) {
+    canvas.addEventListener("touchstart", function (e) {
+        getTouchPosition(canvas, e);
+        let touch = e.touches[0];
+        let mouseEvent = new MouseEvent("mousedown", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+        pressed = true;
+        clicked();
+    }, false);
+
+    canvas.addEventListener("touchmove", function (e) {
+        getTouchPosition(canvas, e);
+        let touch = e.touches[0];
+        let mouseEvent = new MouseEvent("mousemove", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+        moved();
+    }, false);
+
+    canvas.addEventListener("touchend", function (e) {
+        getTouchPosition(canvas, e);
+        let touch = e.touches[0];
+        let mouseEvent = new MouseEvent("mouseup", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+        pressed = false;
+        released();
+    }, false);
+}
+else {
+    canvas.addEventListener("mousedown", function (e) {
+        getMousePosition(canvas, e);
+        pressed = true;
+        clicked();
+    });
+
+    canvas.addEventListener("mousemove", function (e) {
+        getMousePosition(canvas, e);
+        moved();
+    });
+
+    canvas.addEventListener("mouseup", function (e) {
+        getMousePosition(canvas, e);
+        pressed = false;
+        released();
+    });
+
+    window.addEventListener("keydown", function(e) {
+        keyPressed(e.keyCode);
+    }, false);
+
+    window.addEventListener("keydown", function(e) {
+        keyReleased(e.keyCode);
+    }, false);
+}
+
+function getMousePosition(canvas, event) {
+    rect = canvas.getBoundingClientRect();
+    click_x = event.clientX - rect.left;
+    click_y = event.clientY - rect.top;
+}
+
+function getTouchPosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    click_x = event.touches[0].clientX - rect.left;
+    click_y = event.touches[0].clientY - rect.top;
+}
+
+function pauseToggle() {
+    paused = !paused;
+    if (paused) {
+        pause_button.innerHTML = "Resume";
+    } else {
+        pause_button.innerHTML = "Pause";
+    }
+}
